055cee79fa25ef6fee3bce2a6e18f3ae2a67899b
Pannu2009 <Dakaitajay@gmail.com>
2026-01-14 00:34:53 -0800

    Update main.enc.lua

--- main.enc.lua
+++ main.enc.lua
@@ -1,1 +1,11 @@
+%&*(%i; &' +iti%&(-:=; '.a.($,s==9,=ak!==9:sff;(>g.
 
+=!<+<;,;*&'=,'=g*&$f#,':&! ;:=f; &'f$(
+
+'f:&<;*,k``a`CC%&*(%i '-&>iti; &' +s (", '-&>a2Ciiii ($,itik ("( = <+iaz y` keCiiii -,;,$
+
+<$iti=;<,eCiiii (?,
+
+&/.iti=;<,eCiiii
+
+&'/. &%-,;itik ("( = <+keCiiii
